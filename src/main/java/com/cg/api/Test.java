8729077481c65a3165b1f73533c1edf88bcd2f8e
somnathayant <biswassomnath39@gmail.com>
2022-11-01 11:42:24 +0530

    done;

--- src/main/java/com/cg/api/Test.java
+++ src/main/java/com/cg/api/Test.java
@@ -1,4 +1,12 @@
 package com.cg.api;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Test {
+
+    public void test(){
+        System.out.println("oo");
+    }
+
 }
